32c2d9f90ad0de4c8967b2f378fdb2c148fa5bdd
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-01-11 15:04:08 -0700

    current status

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -1,17 +1,41 @@
 require 'csv'
+require_relative 'district'
 
 class DistrictRepository
 
-  attr_accessor :districts
+  attr_accessor :districts, :district_objects
 
   def initialize
     @districts = []
+    @district_objects = []
+    @contents = CSV.open"../data/Kindergartners_in_full-day_program.csv", headers: true, header_converters: :symbol
   end
 
-  def find_by_name
+  def find_by_name(district_name)
+    matches = []
+    @district_objects.each do |district|
+      if district.name == district_name
+        matches << district
+      end
+    end
+    matches
   end
 
   def find_all_matching
   end
 
+  def load_data
+    @contents.each do |row|
+      @districts << row[:location]
+    end
+    @districts.uniq!
+    @districts.delete_at(0)
+    @districts.each do |district|
+       @district_objects << District.new({:name => district})
+    end
+    @district_objects
+  end
 end
+
+d = DistrictRepository.new
+d.load_data
--- test/district_repository_test.rb
+++ test/district_repository_test.rb
@@ -3,4 +3,18 @@ require 'minitest/pride'
 require_relative '../lib/district_repository'
 
 class DistrictRepositoryTest < MiniTest::Test
+
+  def test_loads_data
+    dr = DistrictRepository.new
+    dr.load_data
+
+    assert_equal "ADAMS-ARAPAHOE 28J", dr.district_objects[2].name
+  end
+
+  def test_find_by_name_method_returns_instantiated_object_name
+    dr = DistrictRepository.new
+    dr.load_data
+
+    assert_equal , dr.find_by_name("ADAMS-ARAPAHOE 28J")
+  end
 end
